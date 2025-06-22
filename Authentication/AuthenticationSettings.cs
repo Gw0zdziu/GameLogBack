610f682b79115b6461e4f4e8950fdd98e2c2371d
gw0zdziu <gw0zdziu09@gmail.com>
2025-06-22 15:49:19 +0200

    feat(AuthenticationSettings): add expiry time property to AuthenticationSettings class for accessToken

--- Authentication/AuthenticationSettings.cs
+++ Authentication/AuthenticationSettings.cs
@@ -4,5 +4,6 @@ public class AuthenticationSettings
 {
     public string JwtKey { get; set; }
     public int JwtTokenExpireMinutes { get; set; }
+    public int JwtAccessTokenExpireDays { get; set; }
     public string JwtIssuer { get; set; }
 }
