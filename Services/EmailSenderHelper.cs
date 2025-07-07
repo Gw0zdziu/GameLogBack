ba40fadad769249cf7bc00f8956950b0576fdc92
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-07 14:41:22 +0200

    feat(Services): add service to sending email

---
+++ Services/EmailSenderHelper.cs
@@ -0,0 +1,31 @@
+using System.Net;
+using System.Net.Mail;
+using GameLogBack.Configurations;
+using GameLogBack.Interfaces;
+using Microsoft.Extensions.Options;
+
+namespace GameLogBack.Services;
+
+public class EmailSenderHelper : IEmailSenderHelper
+{
+    private readonly SmtpSettings _smtpSettings;
+
+    public EmailSenderHelper(IOptions<SmtpSettings> smtpSettings)
+    {
+        _smtpSettings = smtpSettings.Value;
+    }
+
+    public void SendEmail(string to, string subject, string message)
+    {
+        MailMessage mailMessage = new MailMessage();
+        mailMessage.From = new MailAddress(_smtpSettings.Address);
+        mailMessage.To.Add(to);
+        mailMessage.Subject = subject;
+        mailMessage.Body = message;
+        SmtpClient smtpClient = new SmtpClient(_smtpSettings.SmtpServer, _smtpSettings.Port);
+        smtpClient.EnableSsl = true;
+        smtpClient.UseDefaultCredentials = false;
+        smtpClient.Credentials = new NetworkCredential(_smtpSettings.Address, _smtpSettings.Password);
+        smtpClient.Send(mailMessage);
+    }
+}
