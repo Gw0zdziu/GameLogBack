88e853ec55238c676fec6a1706181e9845fd1a42
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-08 11:44:49 +0200

    feat(Migrations): update table in database

---
+++ Migrations/20250708094327_AddIsActiveColumnToUsersTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameLogBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsActiveColumnToUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "IsActive",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "False");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "users");
+        }
+    }
+}
