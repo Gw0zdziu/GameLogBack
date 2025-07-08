d8bb1b7fdb84dbe51db7c1a55aea844fc768d1b1
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-08 21:32:19 +0200

    fix(Interfaces): add definition logout and updater user method

--- Interfaces/IUserService.cs
+++ Interfaces/IUserService.cs
@@ -8,6 +8,9 @@ public interface IUserService
     public void RegisterUser(RegisterNewUserDto registerNewUser);
     public LoginResponseDto LoginUser(LoginUserDto loginUserDto);
     public TokenInfoDto GetRefreshToken(TokenInfoDto tokenInfo);
+    public void LogoutUser(string userId);
+    public void UpdateUser(UpdateUserDto updateUserDto, string userId);
+
     public void ResendNewConfirmCode(string userId);
     public void ConfirmUser(ConfirmCodeDto confirmCodeDto);
 }
