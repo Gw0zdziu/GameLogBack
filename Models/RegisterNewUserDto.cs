554eede0a676a074344941f66e19ce207a07b765
gw0zdziu <gw0zdziu09@gmail.com>
2025-07-16 23:19:57 +0200

    refactor(Models): add validations in dtos

--- Models/RegisterNewUserDto.cs
+++ Models/RegisterNewUserDto.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameLogBack.Models;
 
 public class RegisterNewUserDto
 {
     
+    [Required(ErrorMessage = "Username is required")]
+    [MinLength(4,ErrorMessage = "Username must be at least 4 characters long")]
     public string Username { get; set; }
+    
+    [Required(ErrorMessage = "FirstName is required")]
+    [MinLength(3,ErrorMessage = "FirstName must be at least 3 characters long")]
     public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "LastName is required")]   
+    [MinLength(3,ErrorMessage = "LastName must be at least 3 characters long")]
     public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "UserEmail is required")]  
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string UserEmail { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")] 
+    [MinLength(8,ErrorMessage = "Password must be at least 8 characters long")]
     public string Password { get; set; }
 }
--- Models/UpdateUserDto.cs
+++ Models/UpdateUserDto.cs
@@ -1,8 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameLogBack.Models;
 
 public class UpdateUserDto
 {
+    [Required(ErrorMessage = "FirstName is required")]
+    [MinLength(3,ErrorMessage = "FirstName must be at least 3 characters long")]
     public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "LastName is required")]
+    [MinLength(3,ErrorMessage = "LastName must be at least 3 characters long")]
     public string LastName { get; set; }
+    
+    [Required(ErrorMessage = "UserEmail is required")] 
+    [EmailAddress(ErrorMessage = "Please enter a valid email address")]
     public string UserEmail { get; set; }
 }
