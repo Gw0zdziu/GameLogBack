0d3a6a993262bdeb232bd55b1835bfda0451a570
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-04 00:54:30 +0200

    fix(middleware): delete content type in exeception

--- Middlewares/ErrorHandlingMiddleware.cs
+++ Middlewares/ErrorHandlingMiddleware.cs
@@ -13,13 +13,11 @@ public class ErrorHandlingMiddleware: IMiddleware
         catch (NotFoundException e)
         {
             context.Response.StatusCode = 404;
-            context.Response.ContentType = "text/plain";
             await context.Response.WriteAsync(e.Message);
         }
         catch (BadRequestException e)
         {
             context.Response.StatusCode = 400;
-            context.Response.ContentType = "text/plain";
             await context.Response.WriteAsync(e.Message);
 
         }
