6c99a855ed1aa40609a618a5f03a4d9e275931b6
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-07 15:24:55 +0200

    feat(entities): create Categories table

---
+++ Entities/Categories.cs
@@ -0,0 +1,15 @@
+namespace GameLogBack.Entities;
+
+public class Categories
+{
+    public string CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public string CreatedBy { get; set; }
+    public string UpdatedBy { get; set; }
+    public string UserId { get; set; }
+    public virtual Users User { get; set; }
+    
+}
