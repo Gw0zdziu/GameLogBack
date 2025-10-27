dc24e1068ba30bc6a72b10b71fcda366706884be
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-28 00:25:49 +0100

    feat(migrations): create migration and update database

---
+++ Migrations/20251027232052_AddGamesTable.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameLogBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGamesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "games",
+                columns: table => new
+                {
+                    game_id = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    game_name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    created_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    updated_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    category_id = table.Column<string>(type: "character varying(100)", nullable: true),
+                    user_id = table.Column<string>(type: "character varying(100)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_games", x => x.game_id);
+                    table.ForeignKey(
+                        name: "FK_games_categories_category_id",
+                        column: x => x.category_id,
+                        principalTable: "categories",
+                        principalColumn: "category_id");
+                    table.ForeignKey(
+                        name: "FK_games_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "users",
+                        principalColumn: "user_id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_category_id",
+                table: "games",
+                column: "category_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_games_user_id",
+                table: "games",
+                column: "user_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "games");
+        }
+    }
+}
