22f391aee52f69a150f93d3024d8ee29a7337791
gw0zdziu <gw0zdziu09@gmail.com>
2025-10-31 13:00:04 +0100

    feat(interfaces): update interface

--- Interfaces/ICategoryService.cs
+++ Interfaces/ICategoryService.cs
@@ -7,7 +7,7 @@ public interface ICategoryService
 {
     IEnumerable<CategoryDto> GetUserCategories(string userId);
     CategoryDto GetCategory(string categoryId);
-    void CreateCategory(CategoryPostDto categoryPostDto, string userId);
-    void UpdateCategory(CategoryPutDto categoryPutDto, string categoryId, string userId);
+    CategoryDto CreateCategory(CategoryPostDto categoryPostDto, string userId);
+    CategoryDto UpdateCategory(CategoryPutDto categoryPutDto, string categoryId, string userId);
     void DeleteCategory(string categoryId);
 }
