3ff4f3b6f078435a9b9da2cc4fc0c9c513f32283
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-07 11:15:50 +0100

    feat(services): add returned type in method's adding and method's editing in service

--- Services/GameService.cs
+++ Services/GameService.cs
@@ -2,6 +2,8 @@ using GameLogBack.DbContext;
 using GameLogBack.Dtos.Game;
 using GameLogBack.Entities;
 using GameLogBack.Exceptions;
+using GameLogBack.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameLogBack.Services;
 
@@ -22,7 +24,7 @@ public class GameService : IGameService
             throw new BadRequestException("User not found");
         }
 
-        var games = _context.Games.Where(x => x.UserId == userId).Select(x =>
+        var games = _context.Games.Include(x => x.Category).Where(x => x.UserId == userId).Select(x =>
             new GameDto()
             {
                 GameId = x.GameId,
@@ -30,7 +32,9 @@ public class GameService : IGameService
                 UpdatedDate = x.UpdatedDate,
                 UpdatedBy = x.UpdatedBy,
                 CreatedDate = x.CreatedDate,
-                CreatedBy = x.CreatedBy
+                CreatedBy = x.CreatedBy,
+                CategoryId = x.CategoryId,
+                CategoryName = x.Category.CategoryName,
             }
             ).ToList();
         return games;
@@ -38,14 +42,16 @@ public class GameService : IGameService
 
     public GameDto GetGame(string gameId)
     {
-        var game = _context.Games.Where(x => x.GameId == gameId).Select(x => new GameDto()
+        var game = _context.Games.Include(x => x.Category).Where(x => x.GameId == gameId).Select(x => new GameDto()
         {
             GameId = x.GameId,
             GameName = x.GameName,
             UpdatedDate = x.UpdatedDate,
             UpdatedBy = x.UpdatedBy,
             CreatedDate = x.CreatedDate,
-            CreatedBy = x.CreatedBy
+            CreatedBy = x.CreatedBy,
+            CategoryId = x.CategoryId,
+            CategoryName = x.Category.CategoryName,       
         }).FirstOrDefault();
         if (game is null)
         {
@@ -54,7 +60,7 @@ public class GameService : IGameService
         return game;
     }
 
-    public void PostGame(GamePostDto gamePostDto, string userId)
+    public GameDto PostGame(GamePostDto gamePostDto, string userId)
     {
         var isUserExist = _context.Users.Any(x => x.UserId == userId);
         if (!isUserExist)
@@ -63,7 +69,7 @@ public class GameService : IGameService
         }
 
         var isGameNameExist = _context.Games.Any(x => x.GameName == gamePostDto.GameName && x.UserId == userId);
-        if (!isGameNameExist)
+        if (isGameNameExist)
         {
             throw new BadRequestException("Game with this name already exist");
         }
@@ -80,9 +86,22 @@ public class GameService : IGameService
         };
         _context.Games.Add(newGame);
         _context.SaveChanges();
+        var categoryName = _context.Games.Include(x => x.Category).Where(x => x.GameId == newGame.GameId)
+            .Select(x => x.Category.CategoryName).FirstOrDefault();
+        return new GameDto()
+        {
+            GameId = newGame.GameId,
+            GameName = newGame.GameName,
+            CategoryId = newGame.CategoryId, 
+            CategoryName = categoryName,
+            CreatedDate = newGame.CreatedDate,
+            UpdatedDate = newGame.UpdatedDate,
+            CreatedBy = newGame.CreatedBy,
+            UpdatedBy = newGame.UpdatedBy
+        };
     }
 
-    public void PutGame(GamePutDto gamePutDto, string gameId, string userId)
+    public GameDto PutGame(GamePutDto gamePutDto, string gameId, string userId)
     {
         var isUerExist = _context.Users.Any(x => x.UserId == userId);
         if (!isUerExist)
@@ -94,7 +113,7 @@ public class GameService : IGameService
         {
             throw new BadRequestException("Game not found");
         }
-        var gameToUpdate = _context.Games.FirstOrDefault(x => x.GameName == gamePutDto.GameName && x.UserId == userId && x.GameId != gameId);
+        var gameToUpdate = _context.Games.Include(x => x.Category).FirstOrDefault(x => x.GameName == gamePutDto.GameName && x.UserId == userId && x.GameId != gameId);
         if (gameToUpdate is null)
         {
             throw new BadRequestException("Game with this name already exist");
@@ -104,6 +123,17 @@ public class GameService : IGameService
         gameToUpdate.CategoryId = gamePutDto.CategoryId;       
         gameToUpdate.UpdatedDate = DateTime.UtcNow;
         _context.SaveChanges();
+        return new GameDto()
+        {
+            GameId = gameToUpdate.GameId,
+            GameName = gamePutDto.GameName,
+            CategoryId = gamePutDto.CategoryId, 
+            CategoryName = gameToUpdate.Category.CategoryName,
+            CreatedBy = gameToUpdate.CreatedBy,
+            UpdatedBy = gameToUpdate.UpdatedBy,
+            CreatedDate = gameToUpdate.CreatedDate,
+            UpdatedDate = gameToUpdate.UpdatedDate
+        };
     }
     
     public void DeleteGame(string gameId, string userId)
