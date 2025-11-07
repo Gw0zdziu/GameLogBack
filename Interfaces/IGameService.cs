b48559bf3608cb2d7694cb81583ba5b7419c6aa2
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-07 11:15:38 +0100

    feat(interfaces): add returned type in method's adding and method's editing in interface

--- Interfaces/IGameService.cs
+++ Interfaces/IGameService.cs
@@ -1,12 +1,12 @@
 using GameLogBack.Dtos.Game;
 
-namespace GameLogBack.Services;
+namespace GameLogBack.Interfaces;
 
 public interface IGameService
 {
     IEnumerable<GameDto> GetGames(string userId);
     GameDto GetGame(string gameId);
-    void PostGame(GamePostDto gamePostDto, string userId);
-    void PutGame(GamePutDto gamePutDto, string gameId, string userId);
+    GameDto PostGame(GamePostDto gamePostDto, string userId);
+    GameDto PutGame(GamePutDto gamePutDto, string gameId, string userId);
     void DeleteGame(string gameId, string userId);
 }
