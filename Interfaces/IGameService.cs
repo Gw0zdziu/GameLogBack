d53542120ae368a72916c9e5ecc43798f2bdb23c
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-22 00:42:07 +0100

    feat(interfaces): add `GetGamesByUserId` method to `IGameService` interface

--- Interfaces/IGameService.cs
+++ Interfaces/IGameService.cs
@@ -9,4 +9,5 @@ public interface IGameService
     Task<GameDto> PostGame(GamePostDto gamePostDto, string userId);
     Task<GameDto> PutGame(GamePutDto gamePutDto, string gameId, string userId);
     Task DeleteGame(string gameId, string userId);
+    Task<IEnumerable<GameByUserIdDto>> GetGamesByUserId(string userId);
 }
