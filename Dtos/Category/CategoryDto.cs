d718d543f232042cb2e1e57438019c9f18da16e1
gw0zdziu <gw0zdziu09@gmail.com>
2025-11-22 18:22:10 +0100

    feat(dto): add `GamesCount` property to `CategoryByUserIdDto` and `CategoryDto` for improved data representation

--- Dtos/Category/CategoryDto.cs
+++ Dtos/Category/CategoryDto.cs
@@ -9,4 +9,5 @@ public class CategoryDto : CategoryBaseDto
     public DateTime? UpdatedDate { get; set; }
     public string CreatedBy { get; set; }
     public string UpdatedBy { get; set; }
+    public int GamesCount { get; set; }
 }
