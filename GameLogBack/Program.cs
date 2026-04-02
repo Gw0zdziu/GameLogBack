07d4d263ea75c003252104a9befbfb0a1295c56c
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-02 16:31:37 +0200

    chore(environment): switch to environment variables for DB and JWT settings, clean up unused code

--- GameLogBack/Program.cs
+++ GameLogBack/Program.cs
@@ -7,17 +7,25 @@ using GameLogBack.Interfaces;
 using GameLogBack.Middlewares;
 using GameLogBack.Services;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("Postgres");
-//var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+//var connectionString = builder.Configuration.GetConnectionString("Postgres");
+var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 builder.Services.AddDbContext<GameLogDbContext>(options =>
     options.UseNpgsql(connectionString));
-var authenticationSettings = new AuthenticationSettings();
-builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+var authenticationSettings = new AuthenticationSettings()
+{
+    JwtKey = Environment.GetEnvironmentVariable("JWT_KEY"),
+    JwtTokenExpireMinutes = int.Parse(Environment.GetEnvironmentVariable("JWT_TOKEN_EXPIRE_MINUTES") ?? ""),
+    JwtAccessTokenExpireDays = int.Parse(Environment.GetEnvironmentVariable("JWT_ACCESS_TOKEN_EXPIRE_DAYS") ?? ""),
+    JwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER")
+
+};
+/*builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);*/
 builder.Services.Configure<SmtpSettings>(builder.Configuration.GetSection("SmtpSettings"));
 ;
 builder.Services.AddSingleton(authenticationSettings);
@@ -25,7 +33,7 @@ builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AngularApp", policy =>
-        policy.WithOrigins("http://gamelogfront", "http://localhost:4200", "http://localhost:4300",
+        policy.WithOrigins("http://gamelogfront", "http://localhost:4300", "http://localhost:4300",
                 "https://gamelogfront.up.railway.app")
             .AllowAnyMethod()
             .AllowAnyHeader()
@@ -61,6 +69,16 @@ builder.Services.AddScoped<IUtilsService, UtilsService>();
 builder.Services.AddScoped<IEmailSenderHelper, EmailSenderHelper>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IGameService, GameService>();
+/*builder.Services.AddRateLimiter(options =>
+{
+    options.RejectionStatusCode = 429;
+    options.AddFixedWindowLimiter("fixed", limiterOptions =>
+    {
+        limiterOptions.PermitLimit = 5; // Max requests
+        limiterOptions.Window = TimeSpan.FromSeconds(10);
+        limiterOptions.QueueLimit = 0;
+    });
+});*/
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -79,4 +97,11 @@ app.UseCors("AngularApp");
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
+/*using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<GameLogDbContext>();
+    dbContext.Database.Migrate();
+}*/
+/*app.UseRateLimiter();*/
+
 app.Run();
