d84cbf0d5a81ccda21b29b39acaf0255d546c88e
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-17 01:09:29 +0200

    chore(settings): update `JwtAccessTokenExpireDays` to `JwtAccessTokenExpireMinutes` for consistency

--- GameLogBack/Authentication/AuthenticationSettings.cs
+++ GameLogBack/Authentication/AuthenticationSettings.cs
@@ -4,6 +4,6 @@ public class AuthenticationSettings
 {
     public string JwtKey { get; set; }
     public int JwtTokenExpireMinutes { get; set; }
-    public int JwtAccessTokenExpireDays { get; set; }
+    public int JwtAccessTokenExpireMinutes { get; set; }
     public string JwtIssuer { get; set; }
 }
