a5d57cac7c04caaef6314b8494553bae1a79a729
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-20 18:26:30 +0200

    chore(validators): add `GamePostDtoValidator` for game name and year played validation

---
+++ GameLogBack/Validators/Game/GamePostDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using FluentValidation;
+using GameLogBack.Dtos.Game.RequestDto;
+
+namespace GameLogBack.Validators.Game;
+
+public class GamePostDtoValidator : AbstractValidator<GamePostDto>
+{
+    public GamePostDtoValidator()
+    {
+        RuleFor(x => x.GameName).NotEmpty().WithMessage("Game name cannot be empty");
+        RuleFor(x => x.YearPlayed).NotEmpty().WithMessage("Year played cannot be empty");
+    }
+
+}
