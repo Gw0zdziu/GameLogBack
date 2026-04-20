3cbb6c7e0868d681e05c47f0997efa79a84eda56
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-20 18:26:49 +0200

    chore(validators): add `UpdateUserDtoValidator` for user update validation

---
+++ GameLogBack/Validators/User/UpdateUserDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using GameLogBack.Dtos.User;
+
+namespace GameLogBack.Validators.User;
+
+public class UpdateUserDtoValidator : AbstractValidator<UpdateUserDto>
+{
+    public UpdateUserDtoValidator()
+    {
+        RuleFor(x => x.FirstName).NotEmpty().WithMessage("First name is required");
+        RuleFor(x => x.FirstName).MinimumLength(3).WithMessage("Firstname must be at least 3 characters long");
+
+        RuleFor(x => x.LastName).NotEmpty().WithMessage("Last name is required");
+        RuleFor(x => x.LastName).MinimumLength(3).WithMessage("Lastname must be at least 3 characters long");
+
+        RuleFor(x => x.UserEmail).NotEmpty().WithMessage("Email is required");
+        RuleFor(x => x.UserEmail).EmailAddress().WithMessage("Invalid email format");
+    }
+}
