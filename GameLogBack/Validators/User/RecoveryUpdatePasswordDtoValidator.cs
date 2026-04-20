dc51e98dbe30d9b3786b63e888cc6a694c72fcfe
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-20 18:26:59 +0200

    chore(validators): add `RecoveryUpdatePasswordDtoValidator` for password recovery validation

---
+++ GameLogBack/Validators/User/RecoveryUpdatePasswordDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.Data;
+using FluentValidation;
+using GameLogBack.Dtos.User;
+
+namespace GameLogBack.Validators.User;
+
+public class RecoveryUpdatePasswordDtoValidator : AbstractValidator<RecoveryUpdatePasswordDto>
+{
+    public RecoveryUpdatePasswordDtoValidator()
+    {
+        RuleFor(x => x.NewPassword).NotEmpty().WithMessage("New password is required");
+
+        RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Confirm password is required");
+        RuleFor(x => x.NewPassword).Equal(x => x.NewPassword).WithMessage("New password and confirm password do not match");
+
+        RuleFor(x => x.Token).NotEmpty().WithMessage("Token is required");
+        RuleFor(x => x.Token).Length(4).WithMessage("Token is too long");
+    }
+}
