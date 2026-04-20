c560761e3f0bcb4b9c54456fae2803a56e6527bf
Gw0zdziu <gw0zdziu09@gmail.com>
2026-04-20 18:26:10 +0200

    chore(validators): add `CategoryPostDtoValidator` for category name validation

---
+++ GameLogBack/Validators/Category/CategoryPostDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using GameLogBack.Dtos.Category;
+using GameLogBack.Dtos.Category.RequestDto;
+
+namespace GameLogBack.Validators;
+
+public class CategoryPostDtoValidator : AbstractValidator<CategoryPostDto>
+{
+    public CategoryPostDtoValidator()
+    {
+        RuleFor(x => x.CategoryName).NotEmpty().WithMessage("Category name cannot be empty");
+    }
+}
