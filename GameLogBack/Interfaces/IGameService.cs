b1a39195e7ecb356548e2a6f7a62e4eb60b010c5
Gw0zdziu <gw0zdziu09@gmail.com>
2026-05-10 17:45:46 +0200

    refactor(services): update `PostGame` method in `IGameService` to return `Task` instead of `Task<GameDto>`

--- GameLogBack/Interfaces/IGameService.cs
+++ GameLogBack/Interfaces/IGameService.cs
@@ -10,7 +10,7 @@ public interface IGameService
 {
     Task<PaginatedResults<GameDto>> GetGames(string userId, PaginatedQuery paginatedQuery);
     Task<GameDto> GetGame(string gameId);
-    Task<GameDto> PostGame(GamePostDto gamePostDto, string userId);
+    Task PostGame(GamePostDto gamePostDto, string userId);
     Task<GameDto> PutGame(GamePutDto gamePutDto, string gameId, string userId);
     Task DeleteGame(string gameId, string userId);
     Task<IEnumerable<GameByUserIdDto>> GetGamesByUserId(string userId);
