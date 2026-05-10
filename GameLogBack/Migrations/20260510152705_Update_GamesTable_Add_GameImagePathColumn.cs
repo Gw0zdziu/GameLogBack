45c786d12d81f5165dd46417056045e4302cf04b
Gw0zdziu <gw0zdziu09@gmail.com>
2026-05-10 17:46:21 +0200

    chore(migrations): add migration to include optional `GameImagePath` column in `Games` table

---
+++ GameLogBack/Migrations/20260510152705_Update_GamesTable_Add_GameImagePathColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameLogBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_GamesTable_Add_GameImagePathColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GameImagePath",
+                table: "games",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GameImagePath",
+                table: "games");
+        }
+    }
+}
