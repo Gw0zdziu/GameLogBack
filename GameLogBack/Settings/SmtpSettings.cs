2980dd993bc9345ec56226e768bb35afa2d9895d
przemekgwozdz <gw0zdziu09@gmail.com>
2026-06-14 16:14:49 +0200

    chore(config): remove unused `SmtpSettings` class

--- GameLogBack/Settings/SmtpSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace GameLogBack.Configurations;
-
-public class SmtpSettings
-{
-    public string Address { get; set; }
-    public string Password { get; set; }
-    public string SmtpServer { get; set; }
-    public int Port { get; set; }
-}
