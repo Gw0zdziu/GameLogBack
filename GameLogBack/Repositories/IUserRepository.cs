0238129b0ddc13b7300c1c0d192f1b6c3419b797
przemekgwozdz <gw0zdziu09@gmail.com>
2026-07-23 11:57:47 +0200

    feat(repositories): add new methods to `IUserRepository` for retrieving users with related data and confirm codes

--- GameLogBack/Repositories/IUserRepository.cs
+++ GameLogBack/Repositories/IUserRepository.cs
@@ -6,7 +6,9 @@ public interface IUserRepository
 {
     public Task<bool> CheckIfUserExist(string email);
     public Task<Users> GetByEmail(string email);
-    public Task<Users> GetById(string id);
+    public Task<Users> GetById(string Id);
+    public Task<Users> GetUserWithUserLoginsAndCodeRecovery(string userId, string token);
+    public Task<Users> GetUserWithConfirmCode(string userId);
     public Task Create(Users user);
     public Task Update(Users user);
 }
