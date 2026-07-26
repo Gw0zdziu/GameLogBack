677894463627f4069c141c0ac0c8446a83ca5d16
przemekgwozdz <gw0zdziu09@gmail.com>
2026-07-26 14:40:49 +0200

    refactor(auth): use `IRefreshTokenInfoRepository` in `AuthService` to replace direct `DbContext` calls

--- GameLogBack/Services/AuthService.cs
+++ GameLogBack/Services/AuthService.cs
@@ -17,18 +17,20 @@ public class AuthService : IAuthService
     private readonly AuthenticationSettings _authenticationSettings;
     private readonly GameLogDbContext _context;
     private readonly IUserLoginsRepository _userLoginsRepository;
+    private readonly IRefreshTokenInfoRepository _refreshTokenInfoRepository;
     private readonly IPasswordHasher<UserLogins> _passwordHasher;
     private readonly IUtilsService _utilsService;
 
 
     public AuthService(GameLogDbContext context, AuthenticationSettings authenticationSettings,
-        IPasswordHasher<UserLogins> passwordHasher, IUtilsService utilsService, IUserLoginsRepository userLoginsRepository)
+        IPasswordHasher<UserLogins> passwordHasher, IUtilsService utilsService, IUserLoginsRepository userLoginsRepository, IRefreshTokenInfoRepository refreshTokenInfoRepository)
     {
         _context = context;
         _authenticationSettings = authenticationSettings;
         _passwordHasher = passwordHasher;
         _utilsService = utilsService;
         _userLoginsRepository = userLoginsRepository;
+        _refreshTokenInfoRepository = refreshTokenInfoRepository;
     }
 
     public async Task<string> LoginUser(LoginUserDto loginUserDto)
@@ -39,26 +41,25 @@ public class AuthService : IAuthService
         if (result == PasswordVerificationResult.Failed) throw new BadRequestException("Data of login is incorrect");
         var token = _utilsService.GetToken(user, _authenticationSettings.JwtTokenExpireMinutes);
         var refreshToken = _utilsService.GetRefreshToken();
-        var refreshTokenInfo = await _context.RefreshTokens.FirstOrDefaultAsync(x => x.UserId == user.UserId);
+        var refreshTokenInfo = await _refreshTokenInfoRepository.GetByUserId(user.UserId);
         if (refreshTokenInfo is null)
         {
-            _context.RefreshTokens.Add(new RefreshTokenInfo
+            var newRefreshTokenInfo = new RefreshTokenInfo
             {
                 UserId = user.UserId,
                 RefreshTokenId = Guid.NewGuid().ToString(),
                 RefreshToken = refreshToken,
                 ExpiryDate = DateTime.UtcNow.AddMinutes(_authenticationSettings.JwtAccessTokenExpireMinutes)
-            });
+            };
+            await _refreshTokenInfoRepository.Create(newRefreshTokenInfo);
         }
         else
         {
             refreshTokenInfo.ExpiryDate = DateTime.UtcNow.AddMinutes(_authenticationSettings.JwtAccessTokenExpireMinutes);
             refreshTokenInfo.RefreshToken = refreshToken;
+            await _refreshTokenInfoRepository.Update(refreshTokenInfo);
         }
-
-
-        await _context.SaveChangesAsync();
-
+        
         return token;
     }
 
@@ -66,7 +67,7 @@ public class AuthService : IAuthService
     {
         var principal = _utilsService.GetPrincipalFromExpiredToken(tokenInfo);
         var userId = principal.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
-        var refreshTokenInfo = await _context.RefreshTokens.FirstOrDefaultAsync(x => x.UserId == userId);
+        var refreshTokenInfo = await _refreshTokenInfoRepository.GetByUserId(userId);
         if (refreshTokenInfo is null || refreshTokenInfo.ExpiryDate < DateTime.UtcNow)
             throw new BadRequestException("Refresh token is expired");
         var user =  await _userLoginsRepository.GetByUserId(userId).FirstOrDefaultAsync();
@@ -78,9 +79,8 @@ public class AuthService : IAuthService
 
     public async Task LogoutUser(string userId)
     {
-        var refreshTokenInfo = await _context.RefreshTokens.FirstOrDefaultAsync(x => x.UserId == userId);
+        var refreshTokenInfo = await _refreshTokenInfoRepository.GetByUserId(userId);
         if (refreshTokenInfo is null) throw new BadRequestException("");
-        _context.RefreshTokens.Remove(refreshTokenInfo);
-        await _context.SaveChangesAsync();
+        await _refreshTokenInfoRepository.Delete(refreshTokenInfo);
     }
 }
