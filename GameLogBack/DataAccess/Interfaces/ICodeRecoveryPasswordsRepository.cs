ee816fa1a350f8189f04b8fa4b153b31f2177231
Gw0zdziu <gw0zdziu09@gmail.com>
2026-08-11 16:07:33 +0200

    chore(data-access): add `ICodeRecoveryPasswordsRepository` interface for password recovery operations

---
+++ GameLogBack/DataAccess/Interfaces/ICodeRecoveryPasswordsRepository.cs
@@ -0,0 +1,9 @@
+﻿using GameLogBack.Entities;
+
+namespace GameLogBack.DataAccess.Interfaces;
+
+public interface ICodeRecoveryPasswordsRepository
+{
+    public Task<CodeRecoveryPassword> GetByUserId(string userId);
+    public Task Create(CodeRecoveryPassword codeRecoveryPassword);
+}
