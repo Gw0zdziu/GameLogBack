bcaa6c8cc1ab06391962c1958d9522508507962f
przemekgwozdz <gw0zdziu09@gmail.com>
2026-08-17 17:02:51 +0200

    refactor(interfaces): add `Create` and `Update` methods to `ICodeConfirmUsersRepository`

--- GameLogBack/DataAccess/Interfaces/ICodeConfirmUsersRepository.cs
+++ GameLogBack/DataAccess/Interfaces/ICodeConfirmUsersRepository.cs
@@ -5,4 +5,6 @@ namespace GameLogBack.DataAccess.Interfaces;
 public interface ICodeConfirmUsersRepository
 {
     public Task<CodeConfirmUsers> GetByUserId(string userId);
+    public Task Create(CodeConfirmUsers codeConfirmUsers);
+    public Task Update(CodeConfirmUsers codeConfirmUsers);
 }
