678d4745ceb0ceeca3ae0de9608cc2db016dbacd
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-02 22:55:57 +0200

    refactor(services): update `GetByUserId` methods in `ICategoryService` and `IGameService` to support pagination

--- GameLogBack/Interfaces/ICategoryService.cs
+++ GameLogBack/Interfaces/ICategoryService.cs
@@ -13,5 +13,5 @@ public interface ICategoryService
     Task<CategoryDto> CreateCategory(CategoryPostDto categoryPostDto, string userId);
     Task<CategoryDto> UpdateCategory(CategoryPutDto categoryPutDto, string categoryId, string userId);
     Task DeleteCategory(string categoryId);
-    Task<List<CategoryByUserIdDto>> GetCategoriesByUserId(string userId);
+    Task<PaginatedResults<CategoryByUserIdDto>> GetCategoriesByUserId(string userId, PaginatedQuery paginatedQuery);
 }
