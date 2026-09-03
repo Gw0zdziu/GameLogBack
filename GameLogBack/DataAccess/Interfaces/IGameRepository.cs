59373e535adf0453f4f5bcaaf2747c965759897e
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-04 00:41:44 +0200

    fix(repositories): update parameter name in `CheckIfGameExitsById` to `categoryId`

--- GameLogBack/DataAccess/Interfaces/IGameRepository.cs
+++ GameLogBack/DataAccess/Interfaces/IGameRepository.cs
@@ -10,7 +10,7 @@ public interface IGameRepository
     public Task<List<Games>> GetByCategoryId(string id, string userId);
     public Task<Games> GetByGameIdAndUserId(string gameName, string userId);
     public Task<bool> CheckIfGameExists(string gameName, string userId);
-    public Task<bool> CheckIfGameExitsById(string gameId);
+    public Task<bool> CheckIfGameExitsById(string categoryId);
     public Task<bool> CheckIfExistsWithSameName(string gameName, string userId, string gameId);
     public Task Create(Games game);
     public Task Update(Games game);
