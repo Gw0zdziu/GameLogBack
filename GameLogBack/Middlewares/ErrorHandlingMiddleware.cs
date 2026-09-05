807e9c7a58969f75547ddbc154d7a01e5acab4c0
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-05 08:50:09 +0200

    feat(logging): add structured logging to `ErrorHandlingMiddleware`

--- GameLogBack/Middlewares/ErrorHandlingMiddleware.cs
+++ GameLogBack/Middlewares/ErrorHandlingMiddleware.cs
@@ -4,6 +4,13 @@ namespace GameLogBack.Middlewares;
 
 public class ErrorHandlingMiddleware: IMiddleware
 {
+    private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+    public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+    {
+        _logger = logger;
+    }
+
     public async Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
         try
@@ -12,17 +19,27 @@ public class ErrorHandlingMiddleware: IMiddleware
         }
         catch (NotFoundException e)
         {
+            _logger.LogWarning(
+                "Resource not found: {Message} | {Method} | {Path}",
+                e.Message, context.Request.Method, context.Request.Path
+                );
             context.Response.StatusCode = 404;
             await context.Response.WriteAsync(e.Message);
         }
         catch (BadRequestException e)
         {
+            _logger.LogWarning(
+                "Invalid request: {Message} | {Method} | {Path}",
+                e.Message, context.Request.Method, context.Request.Path
+            );
             context.Response.StatusCode = 400;
             await context.Response.WriteAsync(e.Message);
 
         }
         catch (Exception e)
         {
+            _logger.LogError(e, "An unexpected error occurred |{Method} {Path} | TraceId: {TraceId}",
+                context.Request.Method, context.Request.Path, context.TraceIdentifier);
             context.Response.StatusCode = 500;
             await context.Response.WriteAsync("Internal Server Error");
         }
