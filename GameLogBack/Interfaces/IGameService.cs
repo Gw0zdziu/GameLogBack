214758f3a6af8b90c64ea2f774325dfe26266f92
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-15 00:16:12 +0200

    refactor(interfaces): update `PutGame` in `IGameService` to return `void`

--- GameLogBack/Interfaces/IGameService.cs
+++ GameLogBack/Interfaces/IGameService.cs
@@ -11,7 +11,7 @@ public interface IGameService
     Task<PaginatedResults<GameDto>> GetGames(string userId, PaginatedQuery paginatedQuery);
     Task<GameDto> GetGame(string gameId, string userId);
     Task PostGame(GamePostDto gamePostDto, string userId);
-    Task<GameDto> PutGame(GamePutDto gamePutDto, string gameId, string userId);
+    Task PutGame(GamePutDto gamePutDto, string gameId, string userId);
     Task DeleteGame(string gameId, string userId);
     Task<IEnumerable<GameByUserIdDto>> GetGamesByUserId(string userId, PaginatedQuery paginatedQuery);
     Task<IEnumerable<GameByCategoryIdDto>> GetGamesByCategoryId(string categoryId, string userId);
