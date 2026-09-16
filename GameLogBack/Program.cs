355d5ffb2394cc0889f71d4a4edcb0b835ee6859
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-16 23:35:24 +0200

    feat(localization): add request localization middleware and resource-based localization support

--- GameLogBack/Program.cs
+++ GameLogBack/Program.cs
@@ -7,6 +7,7 @@ using GameLogBack.DataAccess.Repositories;
 using GameLogBack.DbContext;
 using GameLogBack.Entities;
 using GameLogBack.Interfaces;
+using GameLogBack.Localization;
 using GameLogBack.Middlewares;
 using GameLogBack.Services;
 using GameLogBack.Settings;
@@ -15,6 +16,7 @@ using GameLogBack.Validators.Category;
 using GameLogBack.Validators.Game;
 using GameLogBack.Validators.User;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Resend;
@@ -29,15 +31,12 @@ Config config;
 BasicAWSCredentials awsCredentials;
 AmazonS3Config s3Config;
 BucketS3 bucketS3;
-
-builder.Services.AddValidatorsFromAssemblyContaining<LoginUserDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryPostDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<CategoryPutDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<GamePostDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<GamePutDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<RegisterNewUserDtoValidator>();
-builder.Services.AddValidatorsFromAssemblyContaining<UpdateUserDtoValidator>();
-builder.Services.AddControllers();
+var supportedCultures = new[] { "pl", "en" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+localizationOptions.RequestCultureProviders.Insert(0, new AcceptLanguageHeaderRequestCultureProvider());
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddResend( o =>
@@ -104,6 +103,15 @@ else
     };
 }
 
+builder.Services.AddValidatorsFromAssemblyContaining<LoginUserDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CategoryPostDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<CategoryPutDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<GamePostDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<GamePutDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<RegisterNewUserDtoValidator>();
+builder.Services.AddValidatorsFromAssemblyContaining<UpdateUserDtoValidator>();
+builder.Services.AddControllers();
+
 builder.Services.AddScoped<ICodeConfirmUsersRepository, CodeConfirmUsersRepository>();
 builder.Services.AddScoped<ICodeRecoveryPasswordsRepository, CodeRecoveryPasswordsRepository>();
 builder.Services.AddScoped<IRefreshTokenInfoRepository, RefreshTokenInfoRepository>();
@@ -120,6 +128,7 @@ builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<IGameService, GameService>();
 builder.Services.AddScoped<IGameBrainApiService, GameBrainApiService>();
 builder.Services.AddScoped<IRailwayBucketService, RailwayBucketService>();
+builder.Services.AddScoped<IAppLocalizer, AppLocalizer>();
 
 builder.Services.AddSingleton(bucketS3);
 builder.Services.AddSingleton(config);
@@ -173,6 +182,7 @@ builder.Services.AddAuthentication(option =>
         ClockSkew = TimeSpan.Zero
     };
 });
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -188,6 +198,7 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.UseRequestLocalization(localizationOptions);
 //app.UseHttpsRedirection();
 app.UseSerilogRequestLogging();
 app.UseMiddleware<ErrorHandlingMiddleware>();
