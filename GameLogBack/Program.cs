1d3c813ab3d83229d3e20db0afdffdbd61adc1c1
przemekgwozdz <gw0zdziu09@gmail.com>
2026-09-16 23:14:36 +0200

    refactor(program): reorder service registrations and cleanup unused code in `Program.cs`

--- GameLogBack/Program.cs
+++ GameLogBack/Program.cs
@@ -10,7 +10,6 @@ using GameLogBack.Interfaces;
 using GameLogBack.Middlewares;
 using GameLogBack.Services;
 using GameLogBack.Settings;
-using GameLogBack.Validators;
 using GameLogBack.Validators.Auth;
 using GameLogBack.Validators.Category;
 using GameLogBack.Validators.Game;
@@ -23,6 +22,14 @@ using Serilog;
 
 
 var builder = WebApplication.CreateBuilder(args);
+string connectionString;
+var gameBrainApiSettings = new GameBrainApiSettings();
+var authenticationSettings = new AuthenticationSettings();
+Config config;
+BasicAWSCredentials awsCredentials;
+AmazonS3Config s3Config;
+BucketS3 bucketS3;
+
 builder.Services.AddValidatorsFromAssemblyContaining<LoginUserDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CategoryPostDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<CategoryPutDtoValidator>();
@@ -31,13 +38,6 @@ builder.Services.AddValidatorsFromAssemblyContaining<GamePutDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<RegisterNewUserDtoValidator>();
 builder.Services.AddValidatorsFromAssemblyContaining<UpdateUserDtoValidator>();
 builder.Services.AddControllers();
-string connectionString;
-var gameBrainApiSettings = new GameBrainApiSettings();
-var authenticationSettings = new AuthenticationSettings();
-var config = new Config();
-BasicAWSCredentials awsCredentials;
-AmazonS3Config s3Config;
-BucketS3 bucketS3;
 if (builder.Environment.IsDevelopment())
 {
     builder.Services.AddResend( o =>
@@ -103,25 +103,38 @@ else
         RecoveryPasswordEndpoint = Environment.GetEnvironmentVariable("RECOVERY_PASSWORD_ENDPOINT"),
     };
 }
+
+builder.Services.AddScoped<ICodeConfirmUsersRepository, CodeConfirmUsersRepository>();
+builder.Services.AddScoped<ICodeRecoveryPasswordsRepository, CodeRecoveryPasswordsRepository>();
+builder.Services.AddScoped<IRefreshTokenInfoRepository, RefreshTokenInfoRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserLoginsRepository, UserLoginsRepository>();
+builder.Services.AddScoped<IGameRepository, GameRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IPasswordHasher<UserLogins>, PasswordHasher<UserLogins>>();
+builder.Services.AddScoped<IUtilsService, UtilsService>();
+builder.Services.AddScoped<IEmailSenderHelper, EmailSenderHelper>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IGameService, GameService>();
+builder.Services.AddScoped<IGameBrainApiService, GameBrainApiService>();
+builder.Services.AddScoped<IRailwayBucketService, RailwayBucketService>();
+
 builder.Services.AddSingleton(bucketS3);
 builder.Services.AddSingleton(config);
 builder.Services.AddSingleton<IAmazonS3>(new AmazonS3Client(awsCredentials, s3Config));
 builder.Services.AddSingleton(gameBrainApiSettings);
 builder.Services.AddDbContext<GameLogDbContext>(options =>
     options.UseNpgsql(connectionString));
+builder.Services.AddSingleton(authenticationSettings);
+
+builder.Services.AddTransient<ErrorHandlingMiddleware>();
+
 builder.Services.AddHttpClient<GameBrainApiService>((client) =>
 {
     client.DefaultRequestHeaders.Add("Accept", "application/json");
 
-});
-builder.Services.AddSingleton(authenticationSettings);
-builder.Services.AddTransient<ErrorHandlingMiddleware>();
-builder.Host.UseSerilog((context, configs) =>
-{
-    configs
-        .MinimumLevel.Information()
-        .Enrich.FromLogContext()
-        .WriteTo.Console();
 });
 builder.Services.AddCors(options =>
 {
@@ -160,38 +173,25 @@ builder.Services.AddAuthentication(option =>
         ClockSkew = TimeSpan.Zero
     };
 });
-builder.Services.AddScoped<ICodeConfirmUsersRepository, CodeConfirmUsersRepository>();
-builder.Services.AddScoped<ICodeRecoveryPasswordsRepository, CodeRecoveryPasswordsRepository>();
-builder.Services.AddScoped<IRefreshTokenInfoRepository, RefreshTokenInfoRepository>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IUserLoginsRepository, UserLoginsRepository>();
-builder.Services.AddScoped<IGameRepository, GameRepository>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IPasswordHasher<UserLogins>, PasswordHasher<UserLogins>>();
-builder.Services.AddScoped<IUtilsService, UtilsService>();
-builder.Services.AddScoped<IEmailSenderHelper, EmailSenderHelper>();
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<IGameService, GameService>();
-builder.Services.AddScoped<IGameBrainApiService, GameBrainApiService>();
-builder.Services.AddScoped<IRailwayBucketService, RailwayBucketService>();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Host.UseSerilog((context, configs) =>
+{
+    configs
+        .MinimumLevel.Information()
+        .Enrich.FromLogContext()
+        .WriteTo.Console();
+});
 
+var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-
 //app.UseHttpsRedirection();
 app.UseSerilogRequestLogging();
 app.UseMiddleware<ErrorHandlingMiddleware>();
 app.UseCors(builder.Environment.IsDevelopment() ? "GameLogDev" : "GameLogProd");
-
 app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
